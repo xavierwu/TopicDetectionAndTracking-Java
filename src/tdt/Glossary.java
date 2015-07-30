f8a1ee892b73f45a0c6eb1f1f1ef6104c0f0e52d
xavierwu <wuzw3@mail2.sysu.edu.cn>
2015-07-30 16:24:02 +0800

    Uh.......

--- src/tdt/Glossary.java
+++ src/tdt/Glossary.java
@@ -3,8 +3,6 @@
  */
 package tdt;
 
-import java.util.HashMap;
-
 /**
  * TODO: class Glossary: Zitong
  * 
@@ -17,31 +15,14 @@ public class Glossary {
 	}
 
 	public int size() {
-		return glossaryStringToInt.size();
+		return 0;
 	}
-
-	public boolean containsWordID(int wordID) {
-		return glossaryIntToString.containsKey(wordID);
+	
+	public boolean containsWordID(int wordID){
+		return false;
 	}
-
-	public String getWord(int wordID) {
-		return glossaryIntToString.get(wordID);
+	
+	public String getWord(int wordID){
+		return "";
 	}
-
-	public Integer getWordID(String word) {
-		return glossaryStringToInt.get(word);
-	}
-
-	public void insertWord(String word) {
-		if (!glossaryStringToInt.containsKey(word)) {
-			int index = size();
-			glossaryIntToString.put(index, word);
-			glossaryStringToInt.put(word, index);
-		}
-	}
-
-	private HashMap<Integer, String> glossaryIntToString = new HashMap<Integer, String>();
-
-	private HashMap<String, Integer> glossaryStringToInt = new HashMap<String, Integer>();
-
 }
