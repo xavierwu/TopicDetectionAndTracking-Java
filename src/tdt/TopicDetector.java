2055d006aca9f5bdc28762bbf06480b9f3be2363
UnfabulousD <dan6024@hotmail.com>
2015-07-31 22:48:21 +0800

    something

--- src/tdt/TopicDetector.java
+++ src/tdt/TopicDetector.java
@@ -5,6 +5,7 @@ package tdt;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Vector;
 
 /**
@@ -27,13 +28,13 @@ class TopicDetector {
 	 * @return
 	 */
 	int doTopicDetection(Vector<Story> corpus) {
-		int numOfTopics = 0;
-		
+		int numOfTopics = 7;
+
 		KMeans(corpus, numOfTopics);
 
 		return numOfTopics;
 	}
-	
+
 	/**
 	 * 
 	 * @param corpus
@@ -41,20 +42,20 @@ class TopicDetector {
 	 */
 	void KMeans(Vector<Story> corpus, int numOfTopics) {
 		Vector<Story> means = new Vector<Story>();
-		
+
 		initMeans(means, corpus, numOfTopics);
-		
+
 		int loopCnt = 10;
-		
-		while (loopCnt < 1) {
+
+		while (loopCnt > 0) {
 			for (int i = 0; i < corpus.size(); i++) {
 				cluster(corpus.get(i), means, numOfTopics);
 			}
-			
+
 			for (int i = 0; i < numOfTopics; i++) {
 				means.set(i, getMean(corpus, i));
 			}
-			
+
 			loopCnt--;
 		}
 	}
@@ -69,36 +70,44 @@ class TopicDetector {
 		// TODO Auto-generated method stub
 		Story mean = new Story();
 		int storyNumOfTopic = 0;
-		
+		HashMap<Integer, Double> tfidfOfMean = new HashMap<Integer, Double>();
+
 		for (int i = 0; i < corpus.size(); i++) {
 			if (corpus.get(i).getTopicID() == topicID) {
 				storyNumOfTopic++;
-				
+
 				HashMap<Integer, Double> tfidf = corpus.get(i).getTfidf();
-				
-				for (Map.Entry<Integer, Double> entry: tfidf.entrySet()) {
-					HashMap<Integer, Double> tfidfOfMean = mean.getTfidf();
-					
-					if (mean.getTfidf().containsKey(entry.getKey())) {
-						 tfidfOfMean.put(entry.getKey(), tfidfOfMean.get(entry.getKey()) + entry.getValue());
-					} else {
+
+				for (Entry<Integer, Double> entry : tfidf.entrySet()) {		
+					System.out.println("key:" + entry.getKey());
+					System.out.println("value:" + entry.getValue());
+
+					try {
+						if (tfidfOfMean.containsKey(entry.getKey())) {
+							tfidfOfMean.put(
+									entry.getKey(),
+									tfidfOfMean.get(entry.getKey())
+											+ entry.getValue());
+						} else {
+							tfidfOfMean.put(entry.getKey(), entry.getValue());
+						}
+					} catch (NullPointerException e) {
 						tfidfOfMean.put(entry.getKey(), entry.getValue());
 					}
-					
 					mean.setTfidf(tfidfOfMean);
 				}
 			}
 		}
-		
-		HashMap<Integer, Double> tfidfOfMean = mean.getTfidf();
-		
-		for (Map.Entry<Integer, Double> entry: tfidfOfMean.entrySet()) {
+
+		// HashMap<Integer, Double> tfidfOfMean = mean.getTfidf();
+
+		for (Map.Entry<Integer, Double> entry : tfidfOfMean.entrySet()) {
 			tfidfOfMean.put(entry.getKey(), entry.getValue() / storyNumOfTopic);
 		}
-		
+
 		mean.setTfidf(tfidfOfMean);
-		
-		return null;
+
+		return mean;
 	}
 
 	/**
@@ -110,10 +119,11 @@ class TopicDetector {
 	private void cluster(Story story, Vector<Story> means, int numOfTopics) {
 		// TODO Auto-generated method stub
 		double maxSimilarity = 0;
-		
+
 		for (int i = 0; i < numOfTopics; i++) {
-			double similarity = StoryLinkDetector.getSimilarity(story, means.get(i));
-			
+			double similarity = StoryLinkDetector.getSimilarity(story,
+					means.get(i));
+
 			if (similarity > maxSimilarity) {
 				maxSimilarity = similarity;
 				story.setTopicID(i);
