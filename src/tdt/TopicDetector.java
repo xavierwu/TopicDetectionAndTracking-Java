f5a3e3950ce20f903f963b4b106644b919897945
UnfabulousD <dan6024@hotmail.com>
2015-07-31 22:06:10 +0800

    something

--- src/tdt/TopicDetector.java
+++ src/tdt/TopicDetector.java
@@ -3,6 +3,8 @@
  */
 package tdt;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 
 /**
@@ -11,13 +13,125 @@ import java.util.Vector;
  * @author Zhaoqi Wang
  */
 class TopicDetector {
+
+	/**
+	 * 
+	 */
 	TopicDetector() {
 
 	}
 
+	/**
+	 * 
+	 * @param corpus
+	 * @return
+	 */
 	int doTopicDetection(Vector<Story> corpus) {
-		int numOfStories = 0;
+		int numOfTopics = 0;
+		
+		KMeans(corpus, numOfTopics);
 
-		return numOfStories;
+		return numOfTopics;
+	}
+	
+	/**
+	 * 
+	 * @param corpus
+	 * @param numOfTopics
+	 */
+	void KMeans(Vector<Story> corpus, int numOfTopics) {
+		Vector<Story> means = new Vector<Story>();
+		
+		initMeans(means, corpus, numOfTopics);
+		
+		int loopCnt = 10;
+		
+		while (loopCnt < 1) {
+			for (int i = 0; i < corpus.size(); i++) {
+				cluster(corpus.get(i), means, numOfTopics);
+			}
+			
+			for (int i = 0; i < numOfTopics; i++) {
+				means.set(i, getMean(corpus, i));
+			}
+			
+			loopCnt--;
+		}
+	}
+
+	/**
+	 * 
+	 * @param corpus
+	 * @param topicID
+	 * @return
+	 */
+	private Story getMean(Vector<Story> corpus, int topicID) {
+		// TODO Auto-generated method stub
+		Story mean = new Story();
+		int storyNumOfTopic = 0;
+		
+		for (int i = 0; i < corpus.size(); i++) {
+			if (corpus.get(i).getTopicID() == topicID) {
+				storyNumOfTopic++;
+				
+				HashMap<Integer, Double> tfidf = corpus.get(i).getTfidf();
+				
+				for (Map.Entry<Integer, Double> entry: tfidf.entrySet()) {
+					HashMap<Integer, Double> tfidfOfMean = mean.getTfidf();
+					
+					if (mean.getTfidf().containsKey(entry.getKey())) {
+						 tfidfOfMean.put(entry.getKey(), tfidfOfMean.get(entry.getKey()) + entry.getValue());
+					} else {
+						tfidfOfMean.put(entry.getKey(), entry.getValue());
+					}
+					
+					mean.setTfidf(tfidfOfMean);
+				}
+			}
+		}
+		
+		HashMap<Integer, Double> tfidfOfMean = mean.getTfidf();
+		
+		for (Map.Entry<Integer, Double> entry: tfidfOfMean.entrySet()) {
+			tfidfOfMean.put(entry.getKey(), entry.getValue() / storyNumOfTopic);
+		}
+		
+		mean.setTfidf(tfidfOfMean);
+		
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param story
+	 * @param means
+	 * @param numOfTopics
+	 */
+	private void cluster(Story story, Vector<Story> means, int numOfTopics) {
+		// TODO Auto-generated method stub
+		double maxSimilarity = 0;
+		
+		for (int i = 0; i < numOfTopics; i++) {
+			double similarity = StoryLinkDetector.getSimilarity(story, means.get(i));
+			
+			if (similarity > maxSimilarity) {
+				maxSimilarity = similarity;
+				story.setTopicID(i);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param means
+	 * @param corpus
+	 * @param numOfTopics
+	 */
+	private void initMeans(Vector<Story> means, Vector<Story> corpus,
+			int numOfTopics) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < numOfTopics; i++) {
+			means.add(corpus.get(i));
+		}
 	}
 }
